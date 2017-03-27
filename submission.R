7f16c073d19d4f48fafeb40ab226754095e84f83
joshi-v <vdashj@gmail.com>
2017-03-27 10:25:52 -0700

    submission for project

--- submission.R
+++ submission.R
@@ -1,26 +1,26 @@
 makecachematrix <- function(x = matrix()) {
   m <- NULL
-  set.Mat <- function(y) {
+  setMat <- function(y) {
     x <<- y
     m <<- NULL
   }
-  get.Mat <- function() x
-  set.Cache <- function(inverse) m <<- inverse
-  get.Cache <- function() m
-  list(set.Mat = set.Mat, 
-       get.Mat = get.Mat,
-       set.Cache = set.Cache,
-       get.Cache = get.Cache)
+  getMat <- function() x
+  setCache <- function(inverse) m <<- inverse
+  getCache <- function() m
+  list(setMat = setMat, 
+       getMat = getMat,
+       setCache = setCache,
+       getCache = getCache)
 }
 
 cacheSolve <- function(x, ...) {
-  m <- x$get.Cache()
+  m <- x$getCache()
   if(!is.null(m)) {
     message("getting cached matrix")
     return(m)
   }
-  dMat <- x$get.Mat()
+  dMat <- x$getMat()
   m <- solve(m, ...)
-  x$set.Cache(m)
+  x$setCache(m)
   m
 }
