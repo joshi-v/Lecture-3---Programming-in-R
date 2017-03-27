73f8512db0e7b1599b95a506c3f816b1894ef0f7
joshi-v <vdashj@gmail.com>
2017-03-27 10:42:17 -0700

    Add files via upload

--- submission.R
+++ submission.R
@@ -1,3 +1,7 @@
+## The makeCacheMatrix creates a list of objects, which are functions. the 
+# set/setinv function pair assigns the matix value to the cached environment. 
+# That hidden env stores the matrix and its inverse.
+
 makecachematrix <- function(x = matrix()) {
   m <- NULL
   setMat <- function(y) {
@@ -12,6 +16,11 @@ makecachematrix <- function(x = matrix()) {
        setCache = setCache,
        getCache = getCache)
 }
+
+## The cacheSolve function takes the chached matrix, and performs operations 
+# on that. If the matrix has a precalculated inverse matrix, then that is returned
+# If the inverse has not been calculated yet, than calculate and store the matrix 
+# inverse in the "cached matrix"
 
 cacheSolve <- function(x, ...) {
   m <- x$getCache()
